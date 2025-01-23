4b3602264b3dad8201b6b7cbddead07ece98c0ff
2500alan <alan.parra@oleofinos.com.mx>
2025-01-23 10:29:02 -0600

    AP-23012025-Produccion

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
 
 	/*Datos de conexion a la base de datos*/
-	define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
-	define('DB_USER', 'alan');//Usuario de tu base de datos
-	define('DB_PASS', 'Zmd294iw');//Contraseña del usuario de la base de datos
-	define('DB_NAME', 'ticketly');//Nombre de la base de datos
+	define('DB_HOST', 'sacyp.mx');//DB_HOST:  generalmente suele ser "127.0.0.1"
+	define('DB_USER', 'level3dw_dev');//Usuario de tu base de datos
+	define('DB_PASS', 'iQHVaIs6iVU%');//Contraseña del usuario de la base de datos
+	define('DB_NAME', 'level3dw_ticketly');//Nombre de la base de datos
 
 	$con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if(!$con){
